697a00938c75441695f6e4e7513192a5a5edbb26
Krishna Teja Chadalavada <102252883+ChKrishnaTeja007@users.noreply.github.com>
2022-12-04 22:39:56 -0700

    Update bubble.js

--- bubble.js
+++ bubble.js
@@ -1,5 +1,5 @@
-var bubbleWidth = 900;
-var bubbleHeight = 450;
+var bubbleWidth = 700;
+var bubbleHeight = 600;
 
 function updateBubbles(data) {
   var svg = d3
@@ -13,15 +13,58 @@ function updateBubbles(data) {
 
   var node = g
     .append("circle")
-    .attr("r", 25)
+    .attr("r", function (d) {
+      return d.key.length < 5 ? 24 : 5 * d.key.length;
+    })
     .attr("cx", bubbleWidth / 2)
     .attr("cy", bubbleHeight / 2)
-    .style("fill", "#69b3a2")
-    .style("fill-opacity", 0.3)
-    .attr("stroke", "#69a2b2")
+    .style("fill", function (_, i) {
+      return colors[i];
+    })
+    .style("fill-opacity", 0.5)
+    .attr("stroke", function (_, i) {
+      return colors[i];
+    })
     .style("stroke-width", 4)
     .on("click", function (d) {
-      selectBubble(d.target.__data__.key);
+      if (!isRegion) selectedRegion = d.target.__data__.key;
+      if (!isOwnership) selectBubble(d.target.__data__.key);
+    })
+    .on("mouseover", function (d) {
+      if (!isOwnership) {
+        d3.select(".lineChart")
+          .selectAll("path")
+          .attr("stroke-width", (_, i, j) => {
+            return Array.from(j)[i].id === d.target.__data__.key ? 4.5 : 0.5;
+          });
+        if (isRegion) {
+          var values = data.filter(
+            (element) => element.key === d.target.__data__.key
+          )[0].values;
+          ind = -1;
+          d3.select(".parallel")
+            .selectAll("path")
+            .attr("stroke-width", (d) => {
+              return values.filter((e) => e.brand === d?.brand).length !== 0
+                ? 4.5
+                : 0.5;
+            })
+            .style("stroke", (d, i) => {
+              if (d === null) return "#000";
+              if (values.filter((e) => e.brand === d?.brand).length !== 0) {
+                ind += 1;
+                return colors[ind];
+              }
+              return colors[values.length + i];
+            });
+        }
+      }
+    })
+    .on("mouseout", function (_) {
+      if (!isOwnership) {
+        d3.select(".lineChart").selectAll("path").attr("stroke-width", 1.5);
+        d3.select(".parallel").selectAll("path").attr("stroke-width", 1.5);
+      }
     });
 
   var simulation = d3
@@ -36,7 +79,7 @@ function updateBubbles(data) {
     .force("charge", d3.forceManyBody().strength(0.5))
     .force(
       "collide",
-      d3.forceCollide().strength(0.01).radius(40).iterations(1)
+      d3.forceCollide().strength(0.01).radius(60).iterations(1)
     );
 
   simulation.nodes(data).on("tick", function (d) {
@@ -54,7 +97,7 @@ function updateBubbles(data) {
         return d.x;
       })
       .attr("y", function (d) {
-        return d.y;
+        return d.y + 2.5;
       })
       .style("text-anchor", "middle")
       .text((d) => {
